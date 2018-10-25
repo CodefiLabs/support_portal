ec861c3af20e3686f3a5981692a0200602b40088
Jon <jonbeal93@gmail.com>
2018-10-24 19:12:52 -0500

    added is deleted field and active scope to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  scope :active, -> { where("is_deleted = ?", false) }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
