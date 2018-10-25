9dbd231e1bdf83729c8c057d340d5bca8d60dd0b
Lisa <lisa.scherer@kellyhawks.org>
2018-10-24 19:48:27 -0500

    add category model
    All of the work I completed is on one branch because I did not understand how to use GitKraken and didn't realize until it was too late that I should've made a branch for each of my Trello cards (assignments).  On this branch, I have set up the Categories and Projects models, created Categories and Projects controller and created routes for both Categories and Projects.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,24 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def udate
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "projects" => "projects#index"
+  # get "projects/:id" => "projects#show"
+  # get "projects/:id/edit" => "projects#edit"
+  # patch "projects/:id" => "projects#update"
+  resources :projects
+
+  # get "categories" => "categories#index"
+  # get "categories/:id" => "categories#show"
+  # get "categories/:id/edit" => "categories#edit"
+  # patch "categories/:id" => "categories#update"
+  resources :categories
+
 end
