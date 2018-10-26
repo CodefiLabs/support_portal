08cdf4e6b23854cee69b7e7b404f3899b37002d9
Tim Barani <timb63701@gmail.com>
2018-10-26 13:07:14 -0500

    Syntax correction

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,5 +1,5 @@
 class Agency < ApplicationRecord
-  has_many: :clients, dependent: :destroy
-  has_many: :users, dependent: :destroy
-  has_many: :priorities, dependent: :destroy
+  has_many :clients, dependent: :destroy
+  has_many :users, dependent: :destroy
+  has_many :priorities, dependent: :destroy
 end
