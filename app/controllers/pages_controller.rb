4c9660ad671692ce26542b76801737f324bff061
Jeff <jeff@recruiterswebsites.com>
2018-10-30 14:14:38 -0500

    Adding Basic User view and list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,11 @@ class PagesController < ApplicationController
   def home
 
   end
-  
+
   def pricing
 
+  end
+  def users
+ @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "pages#index"
   get 'pricing' => "pages#pricing"
   get 'dashboard' => "pages#dashboard"
+  get 'users' => "pages#users"
 
-  
 end
