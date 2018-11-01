7cc70242658105fdeb0991c88363c6c5440cf3e1
Jeff <jeff@recruiterswebsites.com>
2018-11-01 00:30:46 -0500

    Basic Edit form and user list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,5 @@ class PagesController < ApplicationController
   def pricing
 
   end
-  def users
- @users = User.all
- @users = User.paginate(:page => params[:page], :per_page => 10)
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   get 'pricing' => "pages#pricing"
   get 'dashboard' => "pages#dashboard"
-  get 'users' => "pages#users"
+  get 'users' => "users#users"
+  get 'users/:id/edit' => 'users#edit', :as => :user
 
 end
