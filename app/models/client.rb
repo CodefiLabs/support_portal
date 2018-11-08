4c39b92b5a007ebb4a340b6f3c075db01810b361
Llylianna <best_dressed_kids@yahoo.com>
2018-11-07 19:18:02 -0600

    client agency connections

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,8 @@
 class Client < ApplicationRecord
 
+  STATES = %w( AK AL AR AZ CA CO CT DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
+
+
   validates :name, presence: true
   validates :address1, presence: true
   validates :city, presence: true
@@ -7,17 +10,14 @@ class Client < ApplicationRecord
     with: /\A\d{5}(\-?\d{4})?\z/,
     message: "must be a valid U.S. zip code"
     }
-
- STATES = %w( AK AL AR AZ CA CO CT DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
-
   validates :state, inclusion: { in: STATES }
-
-
   validates :phone, format: {
       with: /\A\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*\z/,
       message: "must be a valid phone number"
     }
 
 has_many :agencies, :through => :agencies_clients
-
-end
+has_many :projects
+has_many :categories
+has_many :tickets
+has_many :users
