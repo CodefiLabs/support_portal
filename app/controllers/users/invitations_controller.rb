0e83ec856bcd5699468ad8ad852af47228dd3010
Jeff <jeff@recruiterswebsites.com>
2018-11-08 12:00:45 -0600

    Create company in the invite form some values not making it to the db

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -2,7 +2,17 @@ class Users::InvitationsController < Devise::InvitationsController
   def new
     @users = User.all
     @myusers = User.all
-    @myusers = User.where({ invited_by_id: current_user.id})
+    @myusers = User.where({invited_by_id: current_user.id})
+    @agencies = Agency.all
     super
+  end
+
+  def create
+    super
+  end
+
+  def update_resource_params
+    new_params = ["email", "first_name", "agency_id", "last_name", "phone", "company", "job_title"]
+    params.require(:user).permit(new_params)
   end
 end
